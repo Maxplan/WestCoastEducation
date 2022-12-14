547a132f320c531be5be5c74eaeef2cb5a13cd63
Max <max.planstedt@gmail.com>
2022-12-14 22:20:53 +0100

    Tests, Lib, git setup. Abstract replace interface

---
+++ WestCoastEdu.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using WestCoastEdu.BCL;
+
+namespace WestCoastEdu.Tests;
+
+public class StudentTest
+{
+    [Fact]
+    public void StudentHasSocialSecurityNumberAndAccountIdUponCreation()
+    {
+        
+    }
+}
