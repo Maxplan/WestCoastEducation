1089d5a0a939730bc5b27b96db852d2da1c83cee
Max <max.planstedt@gmail.com>
2022-12-20 20:38:25 +0100

    Setup for web

---
+++ WestCoastEdu.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WestCoastEdu.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
